03174d714b28a7022a6150f305defa9d14098f52
stefank17 <stefank17@ru.is>
2019-05-09 02:21:33 +0000

    profile page, update profile

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
+from candy.models import Candy
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_candy = models.ForeignKey(Candy, on_delete=models.CASCADE)
+    profile_image = models.CharField(max_length=9999)
--- user/views.py
+++ user/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from user.forms.profile_form import ProfileForm
+from user.models import Profile
+
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(data=request.POST)
@@ -9,4 +13,17 @@ def register(request):
             return redirect('login')
     return render(request, 'user/register.html', {
         'form': UserCreationForm()
+    })
+
+def profile(request):
+    profile = Profile.objects.filter(user=request.user).first()
+    if request.method =='POST':
+        form = ProfileForm(instance=profile, data=request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    return render(request, 'user/profile.html', {
+        'form': ProfileForm(instance=profile)
     })
